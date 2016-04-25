e91acd3d1431160149b19cf9a379f922ad4cb6be
Augustomatiello <augusto.mmatiello@gmail.com>
2016-04-25 03:01:05 -0300

    Classe tempo sem encapsulamento

---
+++ src/tempo/tempo.java
@@ -0,0 +1,13 @@
+package tempo;
+
+class time {
+	int hour;
+	int minute;
+	time() {};
+	public static void main (String arg[]) {
+		time t = new time();
+		t.hour = 3;
+		t.minute = 25;
+		System.out.println("The time now is "+t.hour+":"+t.minute);
+	}
+}
