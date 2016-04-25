9b7ec1ed9865925032b3a2d227feb2f1fb577ee2
Augustomatiello <augusto.mmatiello@gmail.com>
2016-04-25 03:10:28 -0300

    Classe tempo com encapsulamento

--- src/tempo/tempo.java
+++ src/tempo/tempo.java
@@ -4,10 +4,14 @@ class time {
 	int hour;
 	int minute;
 	time() {};
+	private void settime(int x, int y){
+		hour = x;
+		minute = y;
+		System.out.println("The time now is "+hour+":"+minute);
+	};
 	public static void main (String arg[]) {
 		time t = new time();
-		t.hour = 3;
-		t.minute = 25;
-		System.out.println("The time now is "+t.hour+":"+t.minute);
+		t.settime(3, 4);
 	}
 }
+
